68d18d31c60fa7fe027691dca4fda1423ecb61ba
Steven Soewignjo <steven.apps.paid@gmail.com>
2023-12-15 18:39:51 +0700

    User Endpoint Adjustment
    Changed the request method of email validation to get

---
+++ frontend-mitra-dashboard/src/pages/login-page/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <>
+            <div className='flex flex-col'>This is login page.</div>
+        </>
+    );
+}
+
+export default LoginPage;
