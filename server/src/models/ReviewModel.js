314639f9ff56db248f9bc126f8af23ac5b83e4d3
Steven Soewignjo <steven.apps.paid@gmail.com>
2023-12-18 00:23:30 +0700

    Preventing Race Condition on creating review (lags) using unique indexes.

--- server/src/models/ReviewModel.js
+++ server/src/models/ReviewModel.js
@@ -1,49 +1,59 @@
 import mongoose from 'mongoose';
 
-const ReviewModel = mongoose.Schema({
-    reviewDate: {
-        type: Date,
-        required: true,
-        default: Date.now,
+const ReviewModel = mongoose.Schema(
+    {
+        reviewDate: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        },
+        reviewDestinationId: {
+            type: String,
+            required: true,
+        },
+        reviewRating: {
+            type: Number,
+            required: true,
+        },
+        reviewComment: {
+            type: String,
+            required: true,
+        },
+        reviewUserId: {
+            type: String,
+            required: true,
+        },
+        reviewPartnerId: {
+            type: String,
+            required: true,
+        },
+        reviewUser: {
+            type: String,
+            required: true,
+        },
+        reviewPartner: {
+            type: String,
+            required: true,
+        },
+        isReplied: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        reviewReply: {
+            type: String,
+            required: false,
+            default: '',
+        },
     },
-    reviewDestinationId: {
-        type: String,
-        required: true,
-    },
-    reviewRating: {
-        type: Number,
-        required: true,
-    },
-    reviewComment: {
-        type: String,
-        required: true,
-    },
-    reviewUserId: {
-        type: String,
-        required: true,
-    },
-    reviewPartnerId: {
-        type: String,
-        required: true,
-    },
-    reviewUser: {
-        type: String,
-        required: true,
-    },
-    reviewPartner: {
-        type: String,
-        required: true,
-    },
-    isReplied: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    reviewReply: {
-        type: String,
-        required: false,
-        default: '',
-    },
-});
+    {
+        indexes: [
+            {
+                unique: true,
+                fields: ['reviewDestinationId', 'reviewUserId'],
+            },
+        ],
+    }
+);
 
 export default mongoose.model('Review', ReviewModel, 'reviews');
